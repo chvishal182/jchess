6a005bc38bb74bb769b1b43d41ac4662cf66926e
cvishaldev <cvishaldev@gmail.com>
2025-11-14 00:14:31 +0530

    Refactor Bishop and Rook classes to extend SlidingPiece; implement column exclusion logic and candidate move offsets

--- src/main/java/com/chelv/chess/engine/pieces/Rook.java
+++ src/main/java/com/chelv/chess/engine/pieces/Rook.java
@@ -1,19 +1,9 @@
 package com.chelv.chess.engine.pieces;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import com.chelv.chess.engine.Alliance;
-import com.chelv.chess.engine.board.Board;
 import com.chelv.chess.engine.board.BoardUtils;
-import com.chelv.chess.engine.board.Move;
-import com.chelv.chess.engine.board.Move.AttackMove;
-import com.chelv.chess.engine.board.Move.MajorMove;
-import com.chelv.chess.engine.board.Tile;
-import com.google.common.collect.ImmutableList;
 
-public class Rook extends Piece {
+public class Rook extends SlidingPiece {
     private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -8, -1, 1, 8 };
 
     Rook(int piecePosition, Alliance pieceAlliance) {
@@ -21,40 +11,17 @@ public class Rook extends Piece {
     }
 
     @Override
-    public Collection<Move> calculateLegalMoves(Board board) {
-        final List<Move> legalMoves = new ArrayList<>();
-        for (final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES) {
-            int candidateDestinationCoordinate = this.piecePosition;
-            while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
-                if (isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset) ||
-                        isEighthColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset)) {
-                    break;
-                }
-                candidateDestinationCoordinate += candidateCoordinateOffset;
-                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
-                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
-                    if (!candidateDestinationTile.isTileOccupied()) {
-                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
-                    } else {
-                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
-                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
-                        if (this.pieceAlliance != pieceAlliance) {
-                            legalMoves.add(
-                                    new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
-                        }
-                        break;// as this coordinate is occupied we cannt slide further in the diagonal
-                    }
-                }
-            }
-        }
-        return ImmutableList.copyOf(legalMoves);
+    public int[] getCandidateMoveOffsets() {
+        return CANDIDATE_MOVE_VECTOR_COORDINATES;
     }
 
-    public static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -1);
+    @Override
+    public boolean isColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return isRookColumnExclusion(currentPosition, candidateOffset);
     }
 
-    public static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == 1);
+    public static boolean isRookColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return (BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -1)) ||
+                (BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == 1));
     }
 }
